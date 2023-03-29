0a1689c206814e01e65b54e30e0f81038d188606
luiznaiper <luiz_naiper@hotmail.com>
2023-03-29 08:27:00 -0300

    :construction: Add category buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
 import Title from './Title'
 import menu from './data'
 import Menu from './Menu'
+import Categories from './Categories'
+
+const tempCategories = menu.map((item) => item.category)
+const tempSet = new Set(tempCategories)
+const allCategories = ['all', ...tempSet]
+
+//Another way to take the allCategories in one line (shorter)
+// const cat = ['all', ...new Set(menu.map((item) => item.category))]
 
 function App() {
   const menuItems = menu
@@ -8,6 +16,7 @@ function App() {
     <main>
       <section className="menu">
         <Title text="our menu" />
+        <Categories categories={allCategories} />
         <Menu items={menuItems} />
       </section>
     </main>
