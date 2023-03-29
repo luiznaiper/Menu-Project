5208f2cb2a32a575de6304039a8e6c552dcf7084
luiznaiper <luiz_naiper@hotmail.com>
2023-03-29 07:54:35 -0300

    :construction: Display menu items

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import Title from './Title'
+import menu from './data'
+import Menu from './Menu'
 
 function App() {
+  const menuItems = menu
   return (
     <main>
       <section className="menu">
         <Title text="our menu" />
+        <Menu items={menuItems} />
       </section>
     </main>
   )
